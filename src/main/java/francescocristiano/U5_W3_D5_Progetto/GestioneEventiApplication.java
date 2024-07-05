e6365a0917b26719aeab6a88e4cc0d913a392328
Francesco Cristiano <frankrys97@gmail.com>
2024-07-05 10:26:41 +0200

    Creates Project

---
+++ src/main/java/francescocristiano/U5_W3_D5_Progetto/GestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package francescocristiano.U5_W3_D5_Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/francescocristiano/U5_W3_D5_Progetto/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package francescocristiano.U5_W3_D5_Progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
